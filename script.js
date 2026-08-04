01ba77eb39f584d189373515839ae76cb2385fca
adhamwail445 <adhamwail112005@gmail.com>
2026-08-05 01:47:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,35 @@
+window.onload = function () {
+
+    setTimeout(() => {
+
+        document.getElementById("welcome").classList.add("hidden");
+
+        document.getElementById("lock").classList.remove("hidden");
+
+    }, 5000);
+
+};
+
+
+
 function unlock() {
 
     let password = document.getElementById("password").value;
 
 
-    if (password === "682005") {
+    if(password === "682005") {
 
 
-        let sound = document.getElementById("unlockSound");
-
-        sound.play();
+        document.getElementById("lock").classList.add("hidden");
 
 
-        document.getElementById("lockScreen").style.opacity = "0";
-
-
-        setTimeout(function(){
-
-
-            document.getElementById("lockScreen").style.display = "none";
-
-
-            document.getElementById("journey").style.display = "flex";
-
-
-            startJourney();
-
-
-        },1000);
-
+        document.getElementById("gate").classList.remove("hidden");
 
 
     } else {
 
 
-        alert("Wrong secret code 💔");
+        alert("Wrong secret ❌");
 
 
     }
@@ -43,76 +39,13 @@ function unlock() {
 
 
 
-
 function startJourney(){
 
 
-    setTimeout(function(){
+    document.getElementById("gate").classList.add("hidden");
 
 
-        document.getElementById("journey").style.display="none";
-
-
-        document.getElementById("memories").style.display="flex";
-
-
-    },4000);
-
-
-
-    setTimeout(function(){
-
-
-        document.getElementById("memories").style.display="none";
-
-
-        document.getElementById("final").style.display="flex";
-
-
-        showFinalMessage();
-
-
-    },12000);
-
-
-
-}
-
-
-
-
-
-function showFinalMessage(){
-
-
-    let message =
-    "Thank you for every beautiful moment. You are a special memory in my life ❤️";
-
-
-    let text = document.getElementById("finalText");
-
-
-    let i = 0;
-
-
-
-    let typing = setInterval(function(){
-
-
-        text.innerHTML += message[i];
-
-
-        i++;
-
-
-        if(i >= message.length){
-
-            clearInterval(typing);
-
-        }
-
-
-    },80);
+    document.getElementById("memories").classList.remove("hidden");
 
 
 }
